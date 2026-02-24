b8ac5d793b9861ae7dceaa3edbbeb7cf6a30aa88
YahKazo <edutsimchaanagkazo@gmail.com>
2026-02-24 13:37:17 +0100

    feat: deleted some redundant files

--- assignments/8-Todo/ignition/modules/Escrow2.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("Escrow2Module", (m) => {
-  const e = m.contract("AuctionContract");
-
-
-  return {e};
-});
